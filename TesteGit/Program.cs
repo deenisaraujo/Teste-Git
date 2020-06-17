fa1e95ced105cb883c923ef7586e100e59919c60
Denis <denisaraujo29@hotmail.com>
2020-06-17 11:05:10 -0300

    duplicado

--- TesteGit/Program.cs
+++ TesteGit/Program.cs
@@ -7,7 +7,10 @@ namespace TesteGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            Console.WriteLine("Bom dia");
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
             Console.ReadLine();
         }
     }
